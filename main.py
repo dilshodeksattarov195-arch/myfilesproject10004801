24e0cb13f0bf524674f3652519652534e11e5c63
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-29 17:25:03 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class tokenVetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = tokenVetchWorker()
+    print("Worker engine initialized.")
